c56b63c5c4c9b7d63c887346d55ed63ece4e8936
Jeden1102 <bum123@o2.pl>
2022-11-09 16:18:56 +0100

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -37087,8 +37087,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kratOS/Projects/OPENSOURCE/laravel-vue-minimal/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kratOS/Projects/OPENSOURCE/laravel-vue-minimal/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\x\htdocs\zlecenia\autka2\laravel-vue-minimal\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\x\htdocs\zlecenia\autka2\laravel-vue-minimal\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
